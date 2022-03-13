9abb9756739edfe5292ab207fe38f69244eb72dd
Lina NOIROT <lnoirot@e1r8p6.42.fr>
2022-03-13 17:09:32 +0100

    protect cose

--- srcs/utils/free_pipex.c
+++ srcs/utils/free_pipex.c
@@ -6,7 +6,7 @@
 /*   By: lnoirot <lnoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 12:29:41 by lnoirot           #+#    #+#             */
-/*   Updated: 2022/03/13 12:35:07 by lnoirot          ###   ########.fr       */
+/*   Updated: 2022/03/13 17:09:12 by lnoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 void	free_pipex(t_pipex *to_free)
 {
-	close(to_free->fd_1);
-	close(to_free->fd_2);
+	if (to_free->fd_1 != -1)
+		close(to_free->fd_1);
+	if (to_free->fd_2 != -1)
+		close(to_free->fd_2);
 	free_str_table(to_free->cmd_1);
 	free_str_table(to_free->cmd_2);
-	close(to_free->fd_pipe[0]);
-	close(to_free->fd_pipe[1]);
+	if (to_free->fd_pipe[0] != -1)
+		close(to_free->fd_pipe[0]);
+	if (to_free->fd_pipe[1] != -1)
+		close(to_free->fd_pipe[1]);
 }
